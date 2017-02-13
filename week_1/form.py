180448208a71d722d36139f66b5235d8970e5808
Otniel Y. Viktorisa <otniel.y.viktorisa@gmail.com>
2017-02-13 16:49:57 +0700

    Finish exercise description and code template

--- week_1/form.py
+++ week_1/form.py
@@ -1,48 +1,48 @@
 class Form:
-   __type = ""
-   __input = ""
-   __button = ""
+    _type = ""
+    _input = ""
+    _button = ""
 
-   def __init__(self, type, input, button):
-      self.__type = type
-      self.__input = input
-      self.__button = button
+    def __init__(self, type, input, button):
+        self._type = type
+        self._input = input
+        self._button = button
 
-   def getType(self):
-      return self.__type.getType()
+    def getType(self):
+        return self._type.getType()
 
-   def getInput(self):
-      return self.__input.getInput()
+    def getInput(self):
+        return self._input.getInput()
 
-   def getButton(self):
-      return self.__button.getButton()
+    def getButton(self):
+        return self._button.getButton()
 
 class InquiryType:
-   __type = ""
+    _type = ""
 
-   def __init__(self):
-      self.__type = "inquiry"
+    def __init__(self):
+        self._type = "inquiry"
 
-   def getType(self):
-      return self.__type
+    def getType(self):
+        return self._type
 
 class InquiryInput:
-   __input = ""
+    _input = ""
 
-   def __init__(self):
-      self.__input = "query"
+    def __init__(self):
+        self._input = "query"
 
-   def getInput(self):
-      return self.__input
+    def getInput(self):
+        return self._input
 
 class InquiryButton:
-   __button = ""
+    _button = ""
 
-   def __init__(self):
-      self.__button = "search"
+    def __init__(self):
+        self._button = "search"
 	
-   def getButton(self):
-      return self.__button
+    def getButton(self):
+        return self._button
 
 ### Mandatory checklist no 2 ###
 #def create_form(factory):
@@ -61,35 +61,35 @@ class InquiryButton:
 #    return builder.form()
 
 def main():
-   inquiryType = InquiryType()
-   inquiryInput = InquiryInput()
-   inquiryButton = InquiryButton()
+    inquiryType = InquiryType()
+    inquiryInput = InquiryInput()
+    inquiryButton = InquiryButton()
 
-   inquiryForm = Form(inquiryType, inquiryInput, inquiryButton)
-   print "Form type",inquiryForm.getType()
-   print "Form input",inquiryForm.getInput()
-   print "Form button",inquiryForm.getButton()
+    inquiryForm = Form(inquiryType, inquiryInput, inquiryButton)
+    print("Form type ", inquiryForm.getType())
+    print("Form input ", inquiryForm.getInput())
+    print("Form button ", inquiryForm.getButton())
 
-   ### Mandatory checklist no 1 ###
-   #entryType = EntryType()
-   #entryInput = EntryInput()
-   #entryButton = EntryButton()
+    ### Mandatory checklist no 1 ###
+    #entryType = EntryType()
+    #entryInput = EntryInput()
+    #entryButton = EntryButton()
 
-   #entryForm = Form(entryType, entryInput, entryButton)
-   #print "Form type",entryForm.getType() #will print "entry"
-   #print "Form input",entryForm.getInput() #will print "data"
-   #print "Form button",entryForm.getButton() #will print "save"
+    #entryForm = Form(entryType, entryInput, entryButton)
+    #print("Form type ", entryForm.getType()) #will print "entry"
+    #print("Form input ", entryForm.getInput()) #will print "data"
+    #print("Form button ", entryForm.getButton()) #will print "save"
 
-   ### Mandatory checklist no 2 ###
-   #inquiryForm = create_form(InquiryFormFactory())
-   #print "Form type",inquiryForm.getType()
-   #print "Form input",inquiryForm.getInput()
-   #print "Form button",inquiryForm.getButton()
+    ### Mandatory checklist no 2 ###
+    #inquiryForm = create_form(InquiryFormFactory())
+    #print("Form type ", inquiryForm.getType())
+    #print("Form input ",inquiryForm.getInput())
+    #print("Form button ",inquiryForm.getButton())
 
-   ### Mandatory checklist no 3 ###
-   #inquiryForm = create_form(InquiryFormBuilder())
-   #print "Form type",inquiryForm.getType()
-   #print "Form input",inquiryForm.getInput()
-   #print "Form button",inquiryForm.getButton()
+    ### Mandatory checklist no 3 ###
+    #inquiryForm = create_form(InquiryFormBuilder())
+    #print("Form type ", inquiryForm.getType())
+    #print("Form input ", inquiryForm.getInput())
+    #print("Form button ", inquiryForm.getButton())
 
 main()
