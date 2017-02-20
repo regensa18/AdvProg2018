9d976df562ff7cdb7fbcabbb31221130eb19f3dc
rian-fitriansyah <rianfitriansyah15@hotmail.com>
2017-02-21 00:16:37 +0700

    Tutorial2-S.py Revised

--- week_2/Tutorial2-S.py
+++ week_2/Tutorial2-S.py
@@ -1,12 +1,12 @@
-#	Mandatory 5: Edit kelas IdnCurrRates menjadi kelas Singleton
-#	Mandatory 6: Edit kelas IdnCurrRates agar bisa dijalankan dengan kelas Borg
+#	Mandatory 9: Edit IdnCurrRates class to a Singleton class
+#	Mandatory 10: Edit IdnCurrRates class to be able to run with Borg class
 class IdnCurrRates (object):
 	def __init__(self,rates):
 		self.rates = rates
 	def __str__(self):
 		return str(self.rates)
 
-#	Mandatory 6: Buat kelas Borg lalu buat kelas SgprCurrRates yang mirip dengan kelas IdnCurrRates
+#	Mandatory 10: Create Borg class then create SgprCurrRates class that has the same method as IdnCurrRates
 
 #class Borg:
 #Implemented in here
@@ -20,17 +20,17 @@ y = IdnCurrRates(20000)
 print ('Rates y: ' + str(y))
 z = IdnCurrRates(30000)
 print ('Rates z: ' + str(z))
-print ('Kondisi x dan y seharusnya menjadi 30000')
+print ('X and Y condition should be 30000')
 print ('Rates x: '+ str(x))
 print ('Rates y: ' + str(y))
 
-# Kode di bawah untuk Mandatory 6
+# Below are codes for Mandatory 10
 # e = SgprCurrRates(40000)
 # print ('Rates x: '+ str(e))
 # f = SgprCurrRates(50000)
 # print ('Rates y: ' + str(f))
 # g = SgprCurrRates(60000)
 # print ('Rates z: ' + str(g))
-# print ('Kondisi e dan f seharusnya menjadi 60000')
+# print ('E and F condition should be 60000')
 # print ('Rates e: '+ str(e))
 # print ('Rates f: ' + str(f))
