ad98c9dbee8aae93196dd494e52bb5518a5b20f2
Hafiyyan Sayyid Fadhlillah <hafiyyan94@gmail.com>
2018-03-10 13:34:16 +0700

    Boilerplate for Spring Ready

---
+++ tutorial-5/src/main/java/hello/HelloController.java
@@ -0,0 +1,15 @@
+package hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String greeting(@RequestParam(name="initial", required=false, defaultValue="Budi") String initial, Model model) {
+        model.addAttribute("initial", initial);
+        return "hello";
+    }
+}
---
+++ tutorial-5/src/test/java/hello/HelloTest.java
@@ -0,0 +1,28 @@
+package hello;
+
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloController.class)
+public class HelloTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    @Test
+    public void sayHello() throws Exception {
+        mockMvc.perform(get("/hello"))
+                .andExpect(content().string(containsString("My Hello for, Budi!")));
+    }
+}
